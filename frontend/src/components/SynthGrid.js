258fceaab8b2b66db16312d0001e11751f88670a
Jeffrey Estrada <jestrada@wpi.edu>
2018-10-05 21:57:13 -0400

    Grid that all play C

--- frontend/src/components/SynthGrid.js
+++ frontend/src/components/SynthGrid.js
@@ -18,11 +18,28 @@ class SynthGrid extends Component {
     this.midiSounds.playChordNow(3, [62], 2.5);
   }
 
+	createTable = () => {
+		let table = []
+		// Outer loop to create rows
+		for (let i = 0; i < 8; i++){
+			let children = []
+			// Inner loop to make td elements
+			for (let j = 0; j < 16; j++){
+				children.push(<td onClick={this.playC.bind(this)}></td>)
+			}
+			table.push(<tr>{children}</tr>)
+		}
+		return table;
+	}
+
   render() {
     return (
       <div>
         <MIDISounds ref={(ref) => (this.midiSounds = ref)} appElementName="root" instruments={[3]} />
-        
+       
+		<table>
+			{this.createTable()}
+		</table>
         <div className='instrument-row'>
           <div className='block' onClick={this.playC.bind(this)}></div>
           <div className='block' onClick={this.playC.bind(this)}></div>
