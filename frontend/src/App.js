899f9ce1d28d2d342766118ad2271f9507ff5921
mariana0pachon <mpachonpuentes@wpi.edu>
2018-10-07 15:48:30 -0400

    Changed order of notes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -50,34 +50,6 @@ class App extends Component {
   }
 
    playSynth=(i,j)=>  {
-      let note;
-      switch(i){
-        case 0:
-          note=60;
-          break;
-        case 1:
-          note=62;
-          break;
-        case 2:
-          note=64;
-          break;
-        case 3:
-          note=65;
-          break;
-        case 4:
-          note=67;
-          break;
-        case 5:
-          note=69;
-          break;
-        case 6:
-          note=71;
-          break;
-        case 7:
-          note=72;
-          break;
-      }
-      //this.midiSounds.playChordNow(3, [note], 2.5);
       var temp = this.state.synthMatrix;
       temp[i][j] = !temp[i][j];
       this.setState({synthMatrix : temp});
@@ -85,22 +57,6 @@ class App extends Component {
   }
 
  playDrums=(i,j)=>  {
-    let drum;
-    switch(i){
-      case 0:
-        drum=16;
-        break;
-      case 1:
-        drum=17;
-        break;
-      case 2:
-        drum=18;
-        break;
-      case 3:
-        drum=19;
-        break;
-    }
-    //this.midiSounds.playDrumsNow([drum]);
     var temp = this.state.drumsMatrix;
     temp[i][j] = !temp[i][j];
     this.setState({drumsMatrix : temp});
@@ -108,34 +64,6 @@ class App extends Component {
   }
 
    playBass=(i,j)=>  {
-      let note;
-      switch(i){
-        case 0:
-          note=60;
-          break;
-        case 1:
-          note=62;
-          break;
-        case 2:
-          note=64;
-          break;
-        case 3:
-          note=65;
-          break;
-        case 4:
-          note=67;
-          break;
-        case 5:
-          note=69;
-          break;
-        case 6:
-          note=71;
-          break;
-        case 7:
-          note=72;
-          break;
-      }
-     // this.midiSounds.playChordNow(130, [note], 1);
       var temp = this.state.bassMatrix;
       temp[i][j] = !temp[i][j];
       this.setState({bassMatrix : temp});
@@ -147,7 +75,6 @@ class App extends Component {
 
       //drums
       let drums=[];
-      console.log('drums', drums);
       if (this.state.drumsMatrix[0][i]){
         drums.push(16);
       }
@@ -164,25 +91,25 @@ class App extends Component {
 
       //synth
      let synths=[130, [], 2.5];
-      if (this.state.synthMatrix[0][i]){synths[1].push(60)}
-      if (this.state.synthMatrix[1][i]){synths[1].push(62)}
-      if (this.state.synthMatrix[2][i]){synths[1].push(64)}
-      if (this.state.synthMatrix[3][i]){synths[1].push(65)}
-      if (this.state.synthMatrix[4][i]){synths[1].push(67)}
-      if (this.state.synthMatrix[5][i]){synths[1].push(69)}
-      if (this.state.synthMatrix[6][i]){synths[1].push(71)}
-      if (this.state.synthMatrix[7][i]){synths[1].push(72)}
+      if (this.state.synthMatrix[0][i]){synths[1].push(72)}
+      if (this.state.synthMatrix[1][i]){synths[1].push(71)}
+      if (this.state.synthMatrix[2][i]){synths[1].push(69)}
+      if (this.state.synthMatrix[3][i]){synths[1].push(67)}
+      if (this.state.synthMatrix[4][i]){synths[1].push(65)}
+      if (this.state.synthMatrix[5][i]){synths[1].push(64)}
+      if (this.state.synthMatrix[6][i]){synths[1].push(62)}
+      if (this.state.synthMatrix[7][i]){synths[1].push(60)}
       //this.melody.push(synths);
 
       let bajos=[20, [], 2.5];
-       if (this.state.bassMatrix[0][i]){bajos[1].push(60)}
-       if (this.state.bassMatrix[1][i]){bajos[1].push(62)}
-       if (this.state.bassMatrix[2][i]){bajos[1].push(64)}
-       if (this.state.bassMatrix[3][i]){bajos[1].push(65)}
-       if (this.state.bassMatrix[4][i]){bajos[1].push(67)}
-       if (this.state.bassMatrix[5][i]){bajos[1].push(69)}
-       if (this.state.bassMatrix[6][i]){bajos[1].push(71)}
-       if (this.state.bassMatrix[7][i]){bajos[1].push(72)}
+       if (this.state.bassMatrix[0][i]){bajos[1].push(72)}
+       if (this.state.bassMatrix[1][i]){bajos[1].push(71)}
+       if (this.state.bassMatrix[2][i]){bajos[1].push(69)}
+       if (this.state.bassMatrix[3][i]){bajos[1].push(67)}
+       if (this.state.bassMatrix[4][i]){bajos[1].push(65)}
+       if (this.state.bassMatrix[5][i]){bajos[1].push(64)}
+       if (this.state.bassMatrix[6][i]){bajos[1].push(62)}
+       if (this.state.bassMatrix[7][i]){bajos[1].push(60)}
       song.push(drums, [synths, bajos]);
 
       this.song[i]=song;
